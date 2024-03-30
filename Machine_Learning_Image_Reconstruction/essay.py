d1e3371b64a1967630918af563da8cb09a851d78
Zhaobo-Wang <wangzhaobo20@gmail.com>
2024-03-30 12:26:04 -0400

    4BF4

---
+++ Machine_Learning_Image_Reconstruction/essay.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# 定义图像空间信号 x
+x = np.array([3, 1])  # 可以更改为任何值
+
+# 定义傅立叶变换矩阵 F
+F = 1/np.sqrt(2) * np.array([[1, 1],
+                             [1, -1]])
+
+# 定义采样掩膜 M
+M = np.array([[1, 0],
+              [0, 0]])
+
+# 计算编码矩阵 E = MF
+E = M.dot(F)
+
+# 应用编码矩阵 E 到 x 上，得到 y
+y = E.dot(x)
+
+print("编码矩阵 E:\n", E)
+print("k空间数据 y:\n", y)
